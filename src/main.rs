d83d215b30868c6fda90dcf9275401df97e7acc3
Canmi <9997200@qq.com>
2025-09-25 20:44:05 +0800

    feat: implement command-line version argument

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 /* src/main.rs */
 
-pub mod common;
-pub mod core;
-pub mod modules;
+use std::env;
+
+mod common;
+mod core;
+mod modules;
 
 #[tokio::main]
 async fn main() {
+    // Handle command-line version argument.
+    if let Some(arg) = env::args().nth(1) {
+        if arg == "version" || arg == "--version" {
+            println!(
+                "{} {} ({} {})",
+                env!("CARGO_PKG_NAME"),
+                env!("CARGO_PKG_VERSION"),
+                env!("GIT_COMMIT_SHORT"),
+                env!("BUILD_DATE")
+            );
+            return; // Exit after printing version.
+        }
+    }
+
+    // If no version arg, start the server.
     core::bootstrap::start().await;
 }
