e555a5a644bf4f4ead63d1b14f1a51eb60304383
Canmi <9997200@qq.com>
2025-09-25 21:42:49 +0800

    feat: Implement configuration management endpoints in the router.

--- src/core/router.rs
+++ src/core/router.rs
@@ -1,8 +1,20 @@
 /* src/core/router.rs */
 
-use crate::core::root::root_handler;
-use axum::{Router, routing::get};
+use crate::{
+    core::root::root_handler,
+    modules::configs::endpoints::{create_handler, delete_handler, get_handler, update_handler},
+};
+use axum::{
+    Router,
+    routing::{delete, get, post, put},
+};
 
+/// The main function to create and configure all application routes.
 pub fn create_router() -> Router {
-    Router::new().route("/", get(root_handler))
+    Router::new()
+        .route("/", get(root_handler))
+        .route("/v1/config/{*key}", get(get_handler))
+        .route("/v1/config/{*key}", post(create_handler))
+        .route("/v1/config/{*key}", put(update_handler))
+        .route("/v1/config/{*key}", delete(delete_handler))
 }
