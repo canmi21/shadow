f758b1e05d2e28be7f1119027e72c9c8d8e38051
Canmi <9997200@qq.com>
2025-09-26 10:22:12 +0800

    refactor: consolidate the main function to start the server and handle the version argument

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,20 @@ mod modules;
 
 #[tokio::main]
 async fn main() {
-    // Handle command-line version argument.
-    if let Some(arg) = env::args().nth(1) {
-        if arg == "version" || arg == "--version" {
-            println!(
-                "{} {} ({} {})",
-                env!("CARGO_PKG_NAME"),
-                env!("CARGO_PKG_VERSION"),
-                env!("GIT_COMMIT_SHORT"),
-                env!("BUILD_DATE")
-            );
-            return; // Exit after printing version.
-        }
-    }
+	// Handle command-line version argument.
+	if let Some(arg) = env::args().nth(1) {
+		if arg == "version" || arg == "--version" {
+			println!(
+				"{} {} ({} {})",
+				env!("CARGO_PKG_NAME"),
+				env!("CARGO_PKG_VERSION"),
+				env!("GIT_COMMIT_SHORT"),
+				env!("BUILD_DATE")
+			);
+			return; // Exit after printing version.
+		}
+	}
 
-    // If no version arg, start the server.
-    core::bootstrap::start().await;
+	// If no version arg, start the server.
+	core::bootstrap::start().await;
 }
