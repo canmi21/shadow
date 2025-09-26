143f06420dc0b3098e612cf58bfad1441360beb0
Canmi <9997200@qq.com>
2025-09-26 10:22:13 +0800

    refactor: improve site owner information initialization

--- src/modules/site/owner.rs
+++ src/modules/site/owner.rs
@@ -5,17 +5,17 @@ use crate::modules::site::requirement::ensure_value_exists;
 
 /// Ensures that default owner values are present in the database.
 pub(super) async fn initialize() -> Result<(), ConfigError> {
-    ensure_value_exists("owner.name", "Canmi".as_bytes()).await?;
-    ensure_value_exists(
-        "owner.quote",
-        "Time will take away everything that belongs to you.".as_bytes(),
-    )
-    .await?;
-    ensure_value_exists(
-        "owner.bio",
-        "Crafting the future in hardware & code.".as_bytes(),
-    )
-    .await?;
+	ensure_value_exists("owner.name", "Canmi".as_bytes()).await?;
+	ensure_value_exists(
+		"owner.quote",
+		"Time will take away everything that belongs to you.".as_bytes(),
+	)
+	.await?;
+	ensure_value_exists(
+		"owner.bio",
+		"Crafting the future in hardware & code.".as_bytes(),
+	)
+	.await?;
 
-    Ok(())
+	Ok(())
 }
